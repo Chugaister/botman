ed3ec8aee0f041cf54cad4c58d6e199db8f6573c
Nazariy Soloviy <nazariy.soloviy@student.manchester.ac.uk>
2023-05-22 17:01:28 +0100

    moved code to webapp folder

---
+++ webapp/wsgi.py
@@ -0,0 +1,15 @@
+from gevent.pywsgi import WSGIServer
+from api import app
+import config
+import ssl
+
+
+http_server = WSGIServer(
+    (config.HOST, config.PORT),
+    app,
+    certfile="certificate.crt",
+    keyfile="private.key",
+    ssl_version=ssl.PROTOCOL_TLS,
+    cert_reqs=ssl.CERT_NONE
+)
+http_server.serve_forever()
