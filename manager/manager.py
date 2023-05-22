bf767c871c37c62af11778e90efcbead6db806c6
Nazariy Soloviy <nazariy.soloviy@student.manchester.ac.uk>
2023-05-22 16:04:57 +0100

    added a manager class

---
+++ manager/manager.py
@@ -0,0 +1,33 @@
+import logging
+from aiogram import Bot, Dispatcher, types
+
+
+class Manager:
+    def __init__(self, bot_tokens):
+        self.bot_dict = {token: (Bot(token=token), Dispatcher()) for token in bot_tokens}
+
+        logging.basicConfig(level=logging.INFO)
+
+        for bot, dp in self.bot_dict.values():
+            self.register_handlers(dp)
+
+    def register_handlers(self, dp):
+        dp.register_message_handler(lambda message: self.echo(message, dp.bot))
+
+    async def handle_message(self, message: types.Message):
+        bot_token = message.bot.token
+        bot, _ = self.bot_dict[bot_token]
+
+        if message.text.startswith('/start'):
+            await self.send_start_message(bot, message.chat.id)
+        else:
+            await self.echo_message(bot, message.chat.id, message.text)
+
+    async def send_start_message(self, bot, chat_id):
+        response_text = "Welcome to the bot! Send me a message and I'll echo it back."
+        await bot.send_message(chat_id=chat_id, text=response_text)
+
+    async def echo_message(self, bot, chat_id, message_text):
+        await bot.send_message(chat_id=chat_id, text=message_text)
+
+    
