cbcc2d2cc6af99be6255b2f38412c7c32ae503b9
Nazariy Soloviy <nazariy.soloviy@student.manchester.ac.uk>
2023-05-26 19:13:31 +0100

    changed api code

--- webapp/api.py
+++ webapp/api.py
@@ -3,7 +3,7 @@ import config
 from dotenv import load_dotenv
 from os import getenv
 from manager.manager import Manager
-from aiogram import types
+from aiogram import types, Dispatcher, Bot
 import asyncio
 
 load_dotenv()
@@ -11,16 +11,7 @@ load_dotenv()
 app = Flask(__name__)
 
 
-def create_webhook_handler(bot_manager):
-    async def handle_webhook(request):
-        data = await request.json()
-        update = types.Update.de_json(data)
-        bot_token = update.message.bot.token
-        _, dp = bot_manager.bot_dict[bot_token]
-        await dp.process_update(update)
-        return 'OK'
 
-    return handle_webhook
 
 class WebhookManager(Manager):
     def __init__(self, bot_tokens, webhook_url):
@@ -32,7 +23,21 @@ class WebhookManager(Manager):
             await bot.set_webhook(self.webhook_url)
 
 async def on_startup():
+    await bot_manager.register_handlers()
     await bot_manager.set_webhook()
+
+def create_webhook_handler(bot_manager):
+    async def handle_webhook(request):
+        data = await request.json()
+        update = types.Update.de_json(data)
+        bot_token = update.message.bot.token
+        bot, dp = bot_manager.bot_dict[bot_token]
+        Dispatcher.set_current(dp)
+        Bot.set_current(bot)
+        await dp.process_update(update)
+        return 'OK'
+
+    return handle_webhook
 
 webhook_url = f'https://20.100.169.126/webhook'
 bot_tokens = [getenv('BOT_TOKEN1')]
@@ -42,7 +47,7 @@ app.route('/webhook', methods=['POST'])(webhook_handler)
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(on_startup(bot_manager))
+    loop.run_until_complete(on_startup())
     app.run(
         host=config.HOST,
         port=config.PORT,
