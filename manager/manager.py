0c4891016b9a32c450b1c494f329da0228c21a6f
Nazariy Soloviy <nazariy.soloviy@student.manchester.ac.uk>
2023-06-01 13:03:57 +0100

    temporarily changed ip, fixed handler registration

--- manager/manager.py
+++ manager/manager.py
@@ -2,7 +2,7 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from data import models
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from web_config.config import PUBLIC_IP
+from web_config.local_config import PUBLIC_IP
 from userbot.handlers import start_handler, req_handler, captcha_confirm, CaptchaStatesGroup
 
 
@@ -18,9 +18,9 @@ class Manager:
             if not self.bot_dict[bot.token]:
                 self.bot_dict[bot.token] = (Bot(token=bot.token), Dispatcher(Bot(token=bot.token), storage=MemoryStorage()))
         for bot, dp in self.bot_dict.values():
-             Bot.set_current(bot)
-             Dispatcher.set_current(dp)        
-            #  dp.register_message_handler(lambda message: self.echo_message(bot, message))
+            #  Bot.set_current(bot)
+            #  Dispatcher.set_current(dp)        
+            #  dp.register_message_handler(lambda message: self.echo_message(Bot.get_current(), message))
              dp.register_message_handler(lambda msg: start_handler(Bot.get_current(), Dispatcher.get_current(), msg), commands="start")
              dp.register_chat_join_request_handler(lambda req, state: req_handler(Bot.get_current(), Dispatcher.get_current(), req, state))
              dp.register_message_handler(lambda msg, state: captcha_confirm(Bot.get_current(), Dispatcher.get_current(), msg, state), state=CaptchaStatesGroup.captcha)    
