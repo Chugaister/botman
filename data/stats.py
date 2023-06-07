88073815f0915985ae6a638e31040507efa6b056
Nazariy Soloviy <nazariy.soloviy@student.manchester.ac.uk>
2023-06-07 19:32:13 +0100

    fixed alg

--- data/stats.py
+++ data/stats.py
@@ -5,8 +5,8 @@ def gen_stats(users: list[User]):
     all_users = len(users)
     active = len([user for user in users if user.status])
     dead = len([user for user in users if not user.status])
-    joined_today = len([user for user in users if (user.join_dt.date() - datetime.now().date()).days <= 1])
-    joined_week = len([user for user in users if (user.join_dt.date() - datetime.now().date()).days <= 7])
-    joined_month = len([user for user in users if (user.join_dt.date() - datetime.now().date()).days <= 30])
+    joined_today = len([user for user in users if (datetime.now().date() - user.join_dt.date()).days <= 1])
+    joined_week = len([user for user in users if (datetime.now().date() - user.join_dt.date()).days <= 7])
+    joined_month = len([user for user in users if (datetime.now().date() -user.join_dt.date()).days <= 30])
 
     return all_users, active, dead, joined_today, joined_week, joined_month
