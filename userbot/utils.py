09bd01c523999b1a5b7f8556c482e84cbec49d29
Nazariy Soloviy <nazarsoloviy2@gmail.com>
2023-08-25 17:20:42 +0100

    added a clean string function

--- userbot/utils.py
+++ userbot/utils.py
@@ -1,5 +1,28 @@
 from data import models
 
+from html.parser import HTMLParser
+import html
+class CustomHTMLParser(HTMLParser):
+    def init(self):
+        super().init()
+        self.result = []
+
+    def handle_starttag(self, tag, attrs):
+        self.result.append(f"<{tag}>")
+
+    def handle_endtag(self, tag):
+        self.result.append(f"</{tag}>")
+
+    def handle_data(self, data):
+        self.result.append(html.escape(data))
+
+def replace_invalid_elements(html_markup):
+    parser = CustomHTMLParser()
+    parser.feed(html_markup)
+    return ''.join(parser.result)
+
+
+
 
 def gen_dynamic_text(text: str, user: models.User) -> str:
     any = user.first_name if user.first_name else user.username
